16d86ffc6ebec3449f4cc66339c4b41742ab6a3c
Danila Danko <br4ch1st0chr0n3@gmail.com>
2021-09-06 22:25:25 +0300

    disable path calculation

--- globals.py
+++ globals.py
@@ -5,13 +5,15 @@ import numpy as np
 np.seterr(all='raise')
 np.random.seed(random_seed)
 
+# control density of paths
 number_of_paths = 3700
 max_path_length = 200
 
 # controls if needed to run ga
+# can be turned off after the first run
 enable_calculations = False
-enable_GA = False
-enable_get_new_paths = False
+enable_GA = True
+enable_get_new_paths = True
 
 board_size = 100
 
