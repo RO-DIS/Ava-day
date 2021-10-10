7c31646a3a8813ad6a59682e6dcbe4b40d892bbe
br4ch1st0chr0n3 <br4ch1st0chr0n3@gmail.com>
2021-10-10 19:57:35 +0300

    add docs, test function

--- Avaday/View/main_window.py
+++ Avaday/View/main_window.py
@@ -4,40 +4,76 @@ import sys
 from PyQt6 import QtCore
 from PyQt6.QtWidgets import QApplication, QDialog, QFileDialog, QVBoxLayout, QWidget
 from Avaday.View.Widgets.drag_n_drop import DragNDropInput
-from Avaday.View.Widgets.generated_picture import GeneratedPicture
+from Avaday.View.Widgets.generated_picture import GeneratedOutput
 from Avaday.View.WidgetHandlers.draw_walks import ImageUpdater
 import shutil
 from pathlib import Path
 from Avaday.config import ROOT_DIR
 
 
-class Custom(QWidget):
+class MainWidget(QWidget):
+    """
+    display ui elements
+
+    S - display ui widgets. Doesn't change at all (only its widgets change)
+
+    O - can be extended and adjusted via built-in functions without need for modification
+
+    L - Preserves interface of QWidget
+
+    I - has a single interface inherited from QWidget
+
+    D - does not use lower-level modules
+    """
     def __init__(self, ui: Ui_Dialog):
         super().__init__()
         # add drag'n'drop widget to dialog
-        self.dnd = DragNDropInput()
+        self.drag_n_drop = DragNDropInput()
         ui.widget_drag_n_drop.setLayout(QVBoxLayout())
-        ui.widget_drag_n_drop.layout().addWidget(self.dnd)
+        ui.widget_drag_n_drop.layout().addWidget(self.drag_n_drop)
 
-        self.upd = ImageUpdater(self.dnd)
+        # add image updater to dialog
+        # connect it to drag'n'drop widget
+        self.updater = ImageUpdater(self.drag_n_drop)
 
         # add generated picture widget to dialog
-        self.pic = GeneratedPicture(self.upd)
-
-        # for fast testing
-        self.dnd.set_image(f"{ROOT_DIR}/resources/user_images/parrot.png")
+        # connect it to image updater
+        self.picture = GeneratedOutput(self.updater)
 
         ui.widget_generated_output.setLayout(QVBoxLayout())
-        ui.widget_generated_output.layout().addWidget(self.pic)
+        ui.widget_generated_output.layout().addWidget(self.picture)
 
-        ui.button_save_as.clicked.connect(self.save_as)
+        self.image_save_dialog = ImageSaveDialog(self.updater)
+
+        ui.button_save_as.clicked.connect(self.image_save_dialog.save_as)
+
+    
+class ImageSaveDialog(QWidget):
+    """
+    handle "Save as" click
+
+    save picture using 
+    
+    S - open file save dialog. Change only on signal from main window
+
+    O - can be extended and adjusted via built-in functions without need for modification
+
+    L - Preserves interface of QWidget
+
+    I - has a single interface inherited from QWidget
+
+    D - does not use lower-level modules
+    """
+    def __init__(self, updater: ImageUpdater):
+        super().__init__()
+        self.updater = updater
 
     def save_as(self, ev):
-        name = Path(self.upd.path_to_generated_image).stem
+        name = Path(self.updater.path_to_generated_image).stem
         file_name, _ = QFileDialog.getSaveFileName(parent=self, caption='Save avatar', directory=f"./{name}.png",
-            initialFilter="Image (*.png *.jpg *jpeg)")
+            initialFilter="Images (*.png *.jpg *jpeg)")
         if file_name:
-            shutil.copy2(src=self.upd.path_to_generated_image, dst = file_name)
+            shutil.copy2(src=self.updater.path_to_generated_image, dst = file_name)
 
 
 
@@ -49,8 +85,22 @@ def run():
         ui.setupUi(Dialog)
         Dialog.show()
 
-        custom = Custom(ui=ui)
+        main = MainWidget(ui=ui)
 
+        sys.exit(app.exec())
+
+def test_run():
+    if (sys.flags.interactive != 1) or not hasattr(QtCore, "PYQT_VERSION"):
+        app = QApplication([])
+        Dialog = QDialog()
+        ui = Ui_Dialog()
+        ui.setupUi(Dialog)
+        Dialog.show()
+
+        main = MainWidget(ui=ui)
+
+        # set initial picture
+        main.drag_n_drop.set_image(f"{ROOT_DIR}/resources/test_images/parrot.png")
         sys.exit(app.exec())
 
 if __name__ == "__main__":
