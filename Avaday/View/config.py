cdabeb2880dfe54f0013d8954f05ec0905d9cbb5
br4ch1st0chr0n3 <br4ch1st0chr0n3@gmail.com>
2021-10-10 18:10:40 +0300

    adjust config

--- Avaday/View/config.py
+++ Avaday/View/config.py
@@ -1,30 +1,31 @@
-# resolution, <= 512
+# Parameters
+
+# View space
+
+# sizes of grid with paths
 BOARD_SIZE = 100
 # maximum height of points after scaling z-coordinates
 BOARD_HEIGHT = 20
-# width of lines on path
+# width of lines
 LINE_WIDTH = 4
-# distance from origin for viewing pictures
+# view point
 VIEW_HEIGHT, VIEW_ELEVATION, VIEW_AZIMUTH = 110, 90, 0
-# 125, 30, -30
-# 125, 40, 180
-
-# side of image
-IMAGE_SIDE = 512
-
+# default background color
 BACKGROUND_COLOR = 'k'
-# BACKGROUND_COLOR = 'w'
-
-# sides of window
+# sides of space view widget
 WINDOW_HEIGHT = 700
 WINDOW_WIDTH = 700
 
+
+# Draw Walks
+
+# valid side of square input image
+IMAGE_SIDE = 512
+# number of paths to read from file with paths
+NUMBER_OF_PATHS = 3700
+
+
+# Image Saver
+
 # side of saved picture
 OUT_IMAGE_SIDE_IN_PIXELS = 670
-
-# seed for random
-RANDOM_SEED = 1828283
-
-# Length of board side
-BOARD_SIZE = 100
-NUMBER_OF_PATHS = 3700
